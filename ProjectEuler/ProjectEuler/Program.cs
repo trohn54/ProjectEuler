7ebf9f2181d00496006f936625c4a8d16b5f0420
Thomas Rohn <trohn54@gmail.com>
2020-05-18 19:42:29 -0500

    Create Multiples of 3 and 5

---
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        public static int MultiplesOf3And5(int max, List<int> Multis)
+        {
+            int sum = 0;
+            for (int i = 0; i < max; i++)
+            {
+                foreach (int multi in Multis)
+                {
+                    if (i % multi == 0) 
+                    { 
+                        sum += i;
+                    }
+                }
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            //List<int> numbers = new List<int>() { 3, 5 };
+            //Console.WriteLine(MultiplesOf3And5(1000, numbers));
+        }
+    }
+}
