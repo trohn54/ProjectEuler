3e1a34dc0506320e369721b67b08f2ad9c2ce24a
Thomas Rohn <trohn54@gmail.com>
2020-06-03 23:00:11 -0500

    challenge 2

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -20,10 +20,35 @@ namespace ProjectEuler
             }
             return sum;
         }
+
+        public static int EvenFibonacciNumbers(int max)
+        {
+            int sum = 0;
+            int fibminus1 = 1;
+            int fibminus2 = 0;
+            int fib = 1;
+
+            while(fib < max)
+            {
+                fibminus2 = fibminus1;
+                fibminus1 = fib;
+                fib = fibminus1 + fibminus2;
+                
+
+                if (fib % 2 == 0)
+                {
+                    sum += fib;
+                }
+            }
+
+            return sum;
+        }
         static void Main(string[] args)
         {
             //List<int> numbers = new List<int>() { 3, 5 };
             //Console.WriteLine(MultiplesOf3And5(1000, numbers));
+
+            Console.WriteLine(EvenFibonacciNumbers(4000000));
         }
     }
 }
