41b20b9bf7f410cfe3204bf2aa6029c8f46e6b7f
Thomas Rohn <trohn54@gmail.com>
2020-06-04 18:27:57 -0500

    start problem 3

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -43,12 +43,45 @@ namespace ProjectEuler
 
             return sum;
         }
+
+        //not my solution
+        public static int LargestPrimeFactor(long number)
+        {
+            List<int> primeFactors = new List<int>();
+
+            for (int factorIterator = 2; factorIterator <= number; factorIterator++)
+            {
+                bool isFactor = number % factorIterator == 0;
+                bool isPrime = true;
+
+                if(isFactor)
+                {
+                    for (int i = 2; i < factorIterator; i++)
+                    {
+                        if (factorIterator % i == 0)
+                        {
+                            isPrime = false;
+                            continue;
+                        }
+                    }
+                }
+
+                if(isFactor && isPrime)
+                {
+                    primeFactors.Add(factorIterator);
+                }
+            }
+
+            return primeFactors[primeFactors.Count - 1];
+        }
         static void Main(string[] args)
         {
             //List<int> numbers = new List<int>() { 3, 5 };
             //Console.WriteLine(MultiplesOf3And5(1000, numbers));
 
-            Console.WriteLine(EvenFibonacciNumbers(4000000));
+            //Console.WriteLine(EvenFibonacciNumbers(4000000));
+            int blah = LargestPrimeFactor(600851475143);
+            Console.WriteLine(blah);
         }
     }
 }
